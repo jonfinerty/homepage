7de59989912f9be28649be743b262acd9b335196
Jonathan Finerty <jonathanfinerty@gmail.com>
2013-02-02 16:13:49 +0000

    basic hover invert tile functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function(){
+
+	$('.tile').hover(function(){
+		$(this).addClass('inverted');
+	}, function(){
+		if ($(this).hasClass('inverted')) {
+			$(this).removeClass('inverted');
+		}
+	});
+
+});
