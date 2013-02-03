33b3f9436ddf5501330e42978b91e7cc51e33153
Jonathan Finerty <jonathanfinerty@gmail.com>
2013-02-03 16:13:28 +0000

    Add rotation of site to info popout

--- js/main.js
+++ js/main.js
@@ -18,5 +18,23 @@ $(document).ready(function(){
 			$('.colour').attr('src', 'img/profile_colour.png');
 		}
 	});
-
+	
+	setTimeout(rotateProject, 8000);
 });
+
+
+function rotateProject(){
+	if ($('.rotate a').attr('href') === 'https://github.com/jf8073/sublime-snake') {
+		$('.rotate a').attr('href', 'http://www.ismymonitorbigenough.com');
+		$('.rotate .info-title').text('IsMyMonitor\nBigEnough.com');
+		$('.rotate .info-tagline').text('Do you have a big enough screen?');
+		$('.rotate .info-desc').text('');
+	} else {
+		$('.rotate a').attr('href', 'https://github.com/jf8073/sublime-snake');
+		$('.rotate .info-title').text('Sublime Snake');
+		$('.rotate .info-tagline').text('A Plugin for Sublime Text 2');
+		$('.rotate .info-desc').text('Lets you play snake with your code.');
+	}
+	
+	setTimeout(rotateProject, 8000);
+}
